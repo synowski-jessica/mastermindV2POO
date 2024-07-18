87b54d747a3fa7be4170571aa35e84eb256ce905
Jessica <synowski.jessica@hotmail.fr>
2024-07-18 22:21:37 +0200

    mise en place du secret code + test en conséquence

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,1 +1,7 @@
-const app = require("../index.js");
+const index = require("../index.js");
+
+describe("secretCode", () => {
+  test("should return a secretCode composed an array with 4 propositions", () => {
+    expect(new index.SecretCode().secretCode().length).toEqual(4);
+  });
+});
