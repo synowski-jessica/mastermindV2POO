0d92b2852d89b10a50c74506f6f7cf9d7a75e9b3
synowski jessica <148389622+synowski-jessica@users.noreply.github.com>
2024-08-15 10:38:37 +0200

    optimisation code checkColor

--- scripts/class/game.js
+++ scripts/class/game.js
@@ -4,12 +4,12 @@ class Game {
   constructor(player, secretCode, numberTry) {
     this.player = player;
     this.secretCode = secretCode;
-    this.attemptCountPlayer = 0;
+    this.attemptCount = 0;
     this.numberTry = numberTry;
   }
 
-  checkStatus() {
-    if (this.attemptCountPlayer > this.numberTry) {
+  isGameContinue() {
+    if (this.attemptCount > this.numberTry) {
       return false;
     } else {
       return true;
--- scripts/class/gameAttempt.js
+++ scripts/class/gameAttempt.js
@@ -4,20 +4,31 @@ class gameAttempt {
   static checkColor(guess, secretCode, numberColor) {
     let goodValuesCount = 0;
     let badValuesCount = 0;
+
+    // Créer un tableau pour suivre les indices déjà comptés dans secretCode
+    const countedIndices = new Array(numberColor).fill(false);
+
+    // Première boucle: compter les "good values" et marquer les indices correspondants
     for (let i = 0; i < numberColor; i++) {
-      //si l'emplacement de la couleur correspond à l'emplacement de la couleur dans la combinaison alors goodvalue+1
       if (guess[i] === secretCode[i]) {
         goodValuesCount += 1;
+        countedIndices[i] = true; // Marque cet indice comme utilisé
       }
-      //si la couleur correspond à un autre emplacement dans la combinaison alors badvalue+1
-      else {
+    }
+
+    // Deuxième boucle: compter les "bad values" uniquement pour les indices non encore comptés
+    for (let i = 0; i < numberColor; i++) {
+      if (guess[i] !== secretCode[i]) {
         for (let j = 0; j < numberColor; j++) {
-          if (guess[i] === secretCode[j]) {
+          if (!countedIndices[j] && guess[i] === secretCode[j]) {
             badValuesCount += 1;
+            countedIndices[j] = true; // Marque cet indice comme utilisé
+            break; // Stoppe la recherche dès que la couleur est trouvée
           }
         }
       }
     }
+
     return { goodValues: goodValuesCount, badValues: badValuesCount };
   }
 }
